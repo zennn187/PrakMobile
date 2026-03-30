999997aa86ed9827b17796133062b5003849a52c
lab151-25 <oza24si@mahasiswa.pcr.ac.id>
2026-03-30 10:10:51 +0700

    Pertemuan 3 (FontImageBinding)

---
+++ app/src/main/java/com/example/ozaapps/Pertemuan_3/ThirdActivity.kt
@@ -0,0 +1,38 @@
+package com.example.ozaapps.Pertemuan_3
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ozaapps.R
+import com.example.ozaapps.databinding.ActivityThirdBinding
+
+class ThirdActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityThirdBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+//        val btnKirim : Button = findViewById(R.id.btnKirim)
+//        val noTujuan : EditText = findViewById(R.id.InputNoTujuan)
+
+
+        binding.btnKirim.setOnClickListener {
+            val nomor = binding.InputNoTujuan.text
+            Toast.makeText(this, "Pesan Berhasil Dikirim ke $nomor", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, ThirdResultActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/ozaapps/Pertemuan_3/ThirdResultActivity.kt
@@ -0,0 +1,21 @@
+package com.example.ozaapps.Pertemuan_3
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ozaapps.R
+
+class ThirdResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_third_result)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
